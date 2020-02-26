f0016e11e3a7dccdef72ef39cb2fbad414fb0541
briangreenedev <brian@cloversoftware.net>
2020-02-26 10:38:24 -0700

    Implemented file input functionality and Material UI library

--- src/components/file-input.js
+++ src/components/file-input.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import {Button} from "@material-ui/core"
+import React, { Component } from 'react'
+import {Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Snackbar} from "@material-ui/core"
 import axios from 'axios'
 import PrivateUrls from '../private/private-urls'
 import AuthUser from "../private/auth-user";
+import LocalStorage from "../utils/local-storage";
+import PrivateKeys from "../private/private-keys";
+import Alert from '@material-ui/lab/Alert';
+import moment from 'moment'
+
+const local = new LocalStorage()
 
 const toBase64 = file => new Promise((resolve, reject) => {
 	const reader = new FileReader()
@@ -11,61 +17,98 @@ const toBase64 = file => new Promise((resolve, reject) => {
 	reader.onerror = err => reject(err)
 })
 
-const onFiles = (files, toggleBackdrop) => {
-	toggleBackdrop()
-	Promise.all(files.map(async file => {
-		debugger
-		return {
-			base64: await toBase64(file),
-			fileName: file.name,
-			fileType: file.type
+class FileInput extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			alreadyUploadedDialogOpen: false
 		}
-	}))
-		.then(fileObjects => {
-			Promise.all(fileObjects.map(fileObject => {
-				let formData = new FormData()
-				formData.set('apikey', AuthUser.apikey)
-				formData.set('jsonpayload', JSON.stringify(fileObject))
-				debugger
-				return axios({
-					method: 'POST',
-					url: PrivateUrls.apiUrl,
-					data: formData
-				})
-			}))
-				.then(fileUploadResults => {
-					debugger
-				})
-				.catch(err => {
-					debugger
-				})
-				.finally(() => {
-					toggleBackdrop()
-				})
-		})
+	}
+
+	onProceedUpload = (files, toggleBackdrop) => {
+		toggleBackdrop()
+
+		let accessibleFileObjects
+		Promise.all(files.map(async file => {
+			return {
+				base64: await toBase64(file),
+				fileName: file.name,
+				fileType: file.type
+			}
+		}))
+			.then(fileObjects => {
+				accessibleFileObjects = fileObjects
+				Promise.all(fileObjects.map(fileObject => {
+					let formData = new FormData()
+					formData.set('apikey', AuthUser.apikey)
+					formData.set('jsonpayload', JSON.stringify(fileObject))
+					return axios({
+						method: 'POST',
+						url: PrivateUrls.apiUrl,
+						data: formData
+					})
+				}))
+					.then(fileUploadResults => {
+						debugger
+						for(let i = 0; i < accessibleFileObjects.length; i++) {
+							for(let j = 0; j < fileUploadResults.length; j++) {
+								if(i === j) {
+									if(fileUploadResults[j].data && fileUploadResults[j].data.Status === 'Good') {
+										local.addFileNameUploaded(accessibleFileObjects[i].fileName)
+										local.addHistoryItem({
+											fileName: accessibleFileObjects[i].fileName,
+											status: 'success',
+											date: Date(),
+											dateString: moment(Date()).format('l')
+										})
+									} else {
+										local.addHistoryItem({
+											fileName: accessibleFileObjects[i].fileName,
+											status: 'error',
+											date: Date(),
+											dateString: moment(Date()).format('l')
+										})
+									}
+								}
+							}
+						}
+						local.cleanupHistory()
+					})
+					.catch(err => {
+						debugger
+					})
+					.finally(() => {
+						toggleBackdrop()
+					})
+			})
+	}
+
+	render () {
+		const {toggleBackdrop, ...rest} = this.props
+		const { alreadyUploadedDialogOpen, snackBarOpen, alertSeverity } = this.state
+		return (
+			<React.Fragment>
+				<Button
+					variant="contained"
+					component="label"
+					size="large"
+					style={{height:'75px', fontSize:'18px'}}
+					fullWidth
+				>
+					Upload Files
+					<input
+						{...rest}
+						type="file"
+						style={{display: 'none'}}
+						multiple
+						onChange={e => {
+							this.onProceedUpload([...e.target.files], toggleBackdrop)
+						}} />
+				</Button>
+			</React.Fragment>
+		)
+	}
 }
-const FileInput = ({ toggleBackdrop, ...rest }) => {
-	debugger
-	return (
-	<React.Fragment>
-		<Button
-			variant="contained"
-			component="label"
-			size="large"
-			style={{height:'75px', fontSize:'18px'}}
-			fullWidth
-		>
-			Upload Files
-			<input
-				{...rest}
-				type="file"
-				style={{display: 'none'}}
-				multiple
-				onChange={e => {
-					onFiles([...e.target.files], toggleBackdrop)
-				}} />
-		</Button>
-	</React.Fragment>
-)}
 
 export default FileInput
